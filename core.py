369fa662110ea6ff5b5ff014e435a8f8f492a43c
HiroIshida <spitfire.docomo@gmail.com>
2020-07-13 01:08:22 +0900

    compute abc

--- core.py
+++ core.py
@@ -9,7 +9,7 @@ def solve(q_start, q_end, n_wp):
     return init_solution
 
 @lru_cache(None)
-def create_K(n_wp, n_dof):
+def construct_K(n_wp, n_dof):
     K_ = np.diag([1]*n_wp)
     for i in range(n_wp):
         for j in range(n_wp):
@@ -18,4 +18,23 @@ def create_K(n_wp, n_dof):
     K = np.kron(K_, np.eye(n_dof))
     return K
 
-mat = create_K(3, 2)
+def construct_Abc(q_start, q_end, n_wp): # A, b and c terms of chomp 
+    n_dof = len(q_start)
+    K = construct_K(n_wp, n_dof)
+    def construct_e():
+        e = -q_start
+        for i in range(n_wp - 2):
+            e = np.hstack((e, np.zeros(n_dof)))
+        e = np.hstack((e, q_end))
+        return e.reshape(n_wp*n_dof, 1)
+
+    e = construct_e()
+    A = K.T.dot(K)
+    b = K.T.dot(e)
+    c = np.linalg.norm(e)**2
+    return A, b, c
+
+s = np.array([0, 0, 0])
+e = np.array([1, 1, 1])
+A, b, c = construct_Abc(s, e, 3)
+
